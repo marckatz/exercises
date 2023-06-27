2b0cb59207e0bcaf2723e0e3db64a360c4019e98
Marc Katz <marc.f.katz@gmail.com>
2023-06-27 12:12:52 -0400

    create basic get/post for adding new workouts to history

--- src/components/History.js
+++ src/components/History.js
@@ -1,24 +1,35 @@
-import React, {useEffect, useState } from "react";
+import React, {Children, useEffect, useState } from "react";
 import Workout from "./Workout";
 import WorkoutForm from "./WorkoutForm";
 
 function History(){
     const [history, setHistory] = useState([])
 
-    function addWorkout(name){
-        return 0
+    function addWorkout(workoutObj){
+        fetch('http://localhost:3001/workouts/',{
+            method:"POST",
+            headers:{"Content-Type": "application/json"},
+            body:JSON.stringify(workoutObj)
+        })
+        .then(r=>r.json())
+        .then(workout => setHistory(h => [...h, workout]))
+        
     }
 
     useEffect(() => {
         fetch('http://localhost:3001/workouts/')
         .then(r => r.json())
-        .then(w => console.log(w))
+        .then(w => setHistory(w))
     }, [])
+
+    const workoutList = history.map(w => {
+        return <Workout id={w.id} key={w.id} exercises={w.exercises} date={w.date} time={w.time} duration={w.duration} />
+    })
 
     return (
         <div>
             <h2>Example History</h2>
-            {history}
+            {workoutList}
             <WorkoutForm addWorkout={addWorkout}/>
         </div>
     )
