dc6ed45b3691cee70eb729cc1863adac71185105
Sebastian Martinez <isaacsoccer235@gmail.com>
2023-06-27 09:47:22 -0500

    more profile stuff

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,30 +1,71 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Modal from './Modal';
-import { useState} from "react"
-import profileData from "./db.json"
+import profileData from "./db.json";
 
-function Profile(){
-    const [openModal, setOpenModal] = useState(false);
+function Profile() {
+  const [openModal, setOpenModal] = useState(false);
+  const [workoutsPerWeek, setWorkoutsPerWeek] = useState(0);
 
-    const {name, streak, totalWorkouts, workoutsPerWeek} = profileData.profile[0]
-    return (
-        <div>
-            <div>
-                <h2 className="profile">Profile</h2>
-                <p>Name: {name} </p>
-                <p>Streak: </p>
-                <p>Total Workouts: </p>
-                <p>Workouts Per Week: </p>
-            </div>
-            <div className= "button-div">
-                <button onClick={() => setOpenModal(true)} className="button-main" >Begin Workout</button>
-                <Modal 
-                open={openModal} 
-                onClose={() => setOpenModal(false)} />
-            </div>
-        </div>
-        
-    )
+  const { name } = profileData.profile[0];
+  const totalWorkouts = profileData.workouts.length;
+
+  useEffect(() => {
+    const workoutsThisWeek = getWorkoutsThisWeek();
+    setWorkoutsPerWeek(workoutsThisWeek.length);
+  }, []);
+
+  function getWorkoutsThisWeek() {
+    const workouts = profileData.workouts;
+    const currentDate = new Date();
+    const startOfWeek = getStartOfWeek(currentDate);
+    const endOfWeek = getEndOfWeek(currentDate);
+
+    const workoutsThisWeek = workouts.filter((workout) => {
+      const workoutDate = new Date(workout.date);
+      return workoutDate >= startOfWeek && workoutDate <= endOfWeek;
+    });
+
+    return workoutsThisWeek;
+  }
+
+  function getStartOfWeek(date) {
+    const day = date.getDay();
+    const diff = date.getDate() - day + (day === 0 ? -6 : 1); 
+    const startOfWeek = new Date(date.setDate(diff));
+    startOfWeek.setHours(0, 0, 0, 0);
+    return startOfWeek;
+  }
+
+  function getEndOfWeek(date) {
+    const startOfWeek = getStartOfWeek(date);
+    const endOfWeek = new Date(startOfWeek);
+    endOfWeek.setDate(startOfWeek.getDate() + 6);
+    endOfWeek.setHours(23, 59, 59, 999);
+    return endOfWeek;
+  }
+
+  const handleBeginWorkout = () => {
+    setOpenModal(true);
+  };
+
+  return (
+    <div>
+      <div>
+        <h2 className="profile">Profile</h2>
+        <p>Name: {name}</p>
+        <p>Streak: </p>
+        <p>Total Workouts: {totalWorkouts}</p>
+        <p>Workouts Per Week: {workoutsPerWeek}</p>
+      </div>
+      <div className="button-div">
+        <button onClick={handleBeginWorkout} className="button-main" >Begin Workout</button>
+        <Modal 
+          open={openModal} 
+          onClose={() => setOpenModal(false)} 
+        />
+      </div>
+    </div>
+  );
 }
 
-export default Profile
+export default Profile;
