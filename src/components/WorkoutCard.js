6a8ac1449f356d04829e78e68818c6a715186f12
Sebastian Martinez <isaacsoccer235@gmail.com>
2023-06-27 20:34:44 -0500

    workout card css

--- src/components/WorkoutCard.js
+++ src/components/WorkoutCard.js
@@ -7,7 +7,7 @@ function WorkoutCard ({workout}) {
         <li key={index}>
             <p><strong>{exercise.name}</strong></p>
             <ul>
-                <li><strong>Sets:</strong> {exercise.sets}</li>
+                <li><strong>Sets:</strong>{exercise.sets}</li>
                 <li><strong>Reps:</strong> {exercise.reps}</li>
             </ul>
         </li>
@@ -15,10 +15,10 @@ function WorkoutCard ({workout}) {
     return (
         <div className="workout-card">
             <h4 className="workout-header"><strong>Workout:</strong> {id}</h4>
-            <h4><strong>Date:</strong> {date}</h4>
+            <h4 className="workout-date"><strong>Date:</strong> {date}</h4>
             <h4 className="workout-time"><strong>Time:</strong> {time}</h4>
-            <h4><strong>Duration:</strong> {duration} minutes</h4>
-            <h4><strong>Exercises:</strong></h4>
+            <h4 className="workout-dur"><strong>Duration:</strong> {duration} minutes</h4>
+            <h4 className="workout-exercises"><strong>Exercises:</strong></h4>
             <ul className="exercise-list">{renderExercises}</ul>
         </div>
 
