df4370eec9870dbc02b6eafe4e4f630a442ae959
Marc Katz <marc.f.katz@gmail.com>
2023-06-29 17:46:20 -0400

    click to expand instuctions on exercises

--- src/components/Exercise.js
+++ src/components/Exercise.js
@@ -1,16 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Excercise({exerciseObj}){
+    const [showInstructions, setShowInstructions] = useState(false)
+
+    function handleShowClick(){
+        setShowInstructions(i => !i)
+    }
+
     const {name, type, muscle, equipment, difficulty, instructions} = exerciseObj
     return (
-        <div className="workout-card">
+        <div className="workout-card" onClick={handleShowClick}>
             <h3>{name}</h3>
             <ul className="exercise-data">
                 <li>Type: {type}</li>
                 <li>Muscle: {muscle}</li>
                 <li>Equipment: {equipment}</li>
                 <li>Difficulty: {difficulty}</li>
-                <li>Instructions: {instructions}</li>
+                {showInstructions?<li>Instructions: {instructions}</li>:null}
             </ul>
         </div>
     )
